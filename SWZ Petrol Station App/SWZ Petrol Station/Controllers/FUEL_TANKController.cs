392cd01585523d81837b9d2a83138ab2a99331bf
KarolWieczorek <karolwieczorek9@o2.pl>
2015-09-11 01:41:48 +0200

    big changes

---
+++ SWZ Petrol Station App/SWZ Petrol Station/Controllers/FUEL_TANKController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SWZ_Petrol_Station;
+
+namespace SWZ_Petrol_Station.Controllers
+{
+    public class FUEL_TANKController : Controller
+    {
+        private DB_9D7C73_karolwieczorek9Entities1 db = new DB_9D7C73_karolwieczorek9Entities1();
+
+        // GET: FUEL_TANK/Details/5
+        public ActionResult TankMonitoring(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            FUEL_TANK fUEL_TANK = new FUEL_TANK();
+            //fUEL_TANK.FUE_PKid
+
+            fUEL_TANK.FUE_NUMBER = id;
+
+            return View(fUEL_TANK);
+        }
+
+        // POST: FUEL_TANK/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TankMonitoring([Bind(Include = "FUE_PKid")] FUEL_TANK fUEL_TANK, int amount, int pressure, int temperature, int id)
+        {
+            fUEL_TANK.FUE_DATE = DateTime.Now;
+            fUEL_TANK.FUE_AMOUNT = amount;
+            fUEL_TANK.FUE__PRESSURE = pressure;
+            fUEL_TANK.FUE_NUMBER = id;
+            fUEL_TANK.FUE_TEMPERATURE = temperature;
+
+            if (ModelState.IsValid)
+            {
+                db.FUEL_TANK.Add(fUEL_TANK);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fUEL_TANK);
+        }
+
+        // GET: FUEL_TANK
+        public ActionResult Index()
+        {
+            return View(db.FUEL_TANK.ToList());
+        }
+
+        // GET: FUEL_TANK/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUEL_TANK fUEL_TANK = db.FUEL_TANK.Find(id);
+            if (fUEL_TANK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUEL_TANK);
+        }
+
+        // GET: FUEL_TANK/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FUEL_TANK/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FUE_PKid,FUE_NUMBER,FUE_DATE,FUE__PRESSURE,FUE_TEMPERATURE,FUE_AMOUNT")] FUEL_TANK fUEL_TANK)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FUEL_TANK.Add(fUEL_TANK);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fUEL_TANK);
+        }
+
+        // GET: FUEL_TANK/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUEL_TANK fUEL_TANK = db.FUEL_TANK.Find(id);
+            if (fUEL_TANK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUEL_TANK);
+        }
+
+        // POST: FUEL_TANK/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FUE_PKid,FUE_NUMBER,FUE_DATE,FUE__PRESSURE,FUE_TEMPERATURE,FUE_AMOUNT")] FUEL_TANK fUEL_TANK)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fUEL_TANK).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fUEL_TANK);
+        }
+
+        // GET: FUEL_TANK/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FUEL_TANK fUEL_TANK = db.FUEL_TANK.Find(id);
+            if (fUEL_TANK == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fUEL_TANK);
+        }
+
+        // POST: FUEL_TANK/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FUEL_TANK fUEL_TANK = db.FUEL_TANK.Find(id);
+            db.FUEL_TANK.Remove(fUEL_TANK);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SWZ Petrol Station App/SWZ Petrol Station/Controllers/RESERVATIONSController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SWZ_Petrol_Station;
+
+namespace SWZ_Petrol_Station.Controllers
+{
+    public class RESERVATIONSController : Controller
+    {
+        private DB_9D7C73_karolwieczorek9Entities1 db = new DB_9D7C73_karolwieczorek9Entities1();
+
+        // GET: RESERVATIONS/Create
+        public ActionResult CreateAReservation()
+        {
+            RESERVATIONS res = new RESERVATIONS();
+            res.CLI_PKid = (int)Session["LogedUserID"];
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN");
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME");
+            return View();
+        }
+
+        // POST: RESERVATIONS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAReservation([Bind(Include = "RES_PKid,RES_DATE_TERM")] RESERVATIONS rESERVATIONS)
+        {
+            var time = rESERVATIONS.RES_DATE_TERM;
+            var list = db.RESERVATIONS.Where(r => r.RES_DATE_TERM >= time);// && r.RES_DATE_TERM.AddHours(1) < time);
+            if(list.Count() == 0)
+            {
+                rESERVATIONS.RES_RESERVATION_DATE = DateTime.Now;
+                rESERVATIONS.CLI_PKid = (int)Session["LogedUserID"];
+                rESERVATIONS.POS_PKid = 1;
+
+                if (ModelState.IsValid)
+                {
+                    db.RESERVATIONS.Add(rESERVATIONS);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN", rESERVATIONS.CLI_PKid);
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME", rESERVATIONS.POS_PKid);
+
+            return View(rESERVATIONS);
+        }
+
+        // GET: RESERVATIONS
+        public ActionResult Index()
+        {
+            var rESERVATIONS = db.RESERVATIONS.Include(r => r.CLIENTS).Include(r => r.POSITION_SERVICE);
+            return View(rESERVATIONS.ToList());
+        }
+
+        // GET: RESERVATIONS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RESERVATIONS rESERVATIONS = db.RESERVATIONS.Find(id);
+            if (rESERVATIONS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rESERVATIONS);
+        }
+
+        // GET: RESERVATIONS/Create
+        public ActionResult Create()
+        {
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN");
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME");
+            return View();
+        }
+
+        // POST: RESERVATIONS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RES_PKid,RES_RESERVATION_DATE,RES_DATE_TERM,POS_PKid,CLI_PKid")] RESERVATIONS rESERVATIONS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RESERVATIONS.Add(rESERVATIONS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN", rESERVATIONS.CLI_PKid);
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME", rESERVATIONS.POS_PKid);
+            return View(rESERVATIONS);
+        }
+
+        // GET: RESERVATIONS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RESERVATIONS rESERVATIONS = db.RESERVATIONS.Find(id);
+            if (rESERVATIONS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN", rESERVATIONS.CLI_PKid);
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME", rESERVATIONS.POS_PKid);
+            return View(rESERVATIONS);
+        }
+
+        // POST: RESERVATIONS/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RES_PKid,RES_RESERVATION_DATE,RES_DATE_TERM,POS_PKid,CLI_PKid")] RESERVATIONS rESERVATIONS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rESERVATIONS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CLI_PKid = new SelectList(db.CLIENTS, "CLI_PKid", "CLI_LOGIN", rESERVATIONS.CLI_PKid);
+            ViewBag.POS_PKid = new SelectList(db.POSITION_SERVICE, "POS_PKid", "POS_NAME", rESERVATIONS.POS_PKid);
+            return View(rESERVATIONS);
+        }
+
+        // GET: RESERVATIONS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RESERVATIONS rESERVATIONS = db.RESERVATIONS.Find(id);
+            if (rESERVATIONS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rESERVATIONS);
+        }
+
+        // POST: RESERVATIONS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RESERVATIONS rESERVATIONS = db.RESERVATIONS.Find(id);
+            db.RESERVATIONS.Remove(rESERVATIONS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
